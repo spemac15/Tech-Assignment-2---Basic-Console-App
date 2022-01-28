08858794f7f0d58aab2c9cfb26ec34128a2f9d9e
spenc <spenc@SPENCER-PC>
2022-01-28 13:31:01 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// McCloskey, Spencer
+using System;
+
+namespace Tech_Assignment_2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Keep moving forward!");
+        }
+    }
+}
